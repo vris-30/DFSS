7e8e98c2eaef85696eef1492e49db3959c2e7afe
vrindas <vrindas@amdocs.com>
2024-07-31 15:45:54 +0530

    created a NewFileServer function

---
+++ server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"sync"
+
+	"https://github.com/vris-30/DFSS/tree/main/p2p"
+)
+
+type FileServerOpts struct {
+	ID                string
+	EncKey            []byte
+	StorageRoot       string
+	PathTransformFunc PathTransformFunc
+	Transport         p2p.Transport
+	BootstrapNodes    []string
+}
+
+type FileServer struct {
+	FileServerOpts
+
+	peerLock sync.Mutex
+	peers    map[string]p2p.Peer
+
+	store  *Store
+	quitch chan struct{}
+}
+
+func NewFileServer(opts FileServerOpts) *FileServer {
+	storeOpts := StoreOpts{
+		Root:              opts.StorageRoot,
+		PathTransformFunc: opts.PathTransformFunc,
+	}
+
+	if len(opts.ID) == 0 {
+		opts.ID = generateID()
+	}
+
+	return &FileServer{
+		FileServerOpts: opts,
+		store:          NewStore(storeOpts),
+		quitch:         make(chan struct{}),
+		peers:          make(map[string]p2p.Peer),
+	}
+}
